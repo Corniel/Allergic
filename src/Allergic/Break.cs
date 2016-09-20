dbe5948a6cde605ba1dee84d848d13fc647cfed5
Corniel Nobel <mail@corniel.nl>
2016-09-20 12:55:40 +0200

    Launch the debugger if not attached yet.

--- src/Allergic/Break.cs
+++ src/Allergic/Break.cs
@@ -5,11 +5,14 @@ namespace Allergic
 	/// <summary>Debugger.Break helper.</summary>
 	public static class Break
 	{
-		/// <summary>Equivalent of <code>Debugger.Break();</code>.</summary>
+		/// <summary>Equivalent of <code>Debugger.Break();</code> but launches
+		/// the debugger if not attached yet.
+		/// </summary>
 		[DebuggerStepThrough]
 		public static void Now() { If(true); }
 
-		/// <summary>Equivalent of a conditional breakpoint.</summary>
+		/// <summary>Equivalent of a conditional breakpoint but launches the
+		/// debugger if not attached yet.</summary>
 		/// <remarks>
 		/// The processing of conditional breakpoints is extremely slow, this is quick.
 		/// </remarks>
@@ -18,7 +21,14 @@ namespace Allergic
 		{
 			if (condition)
 			{
-				Debugger.Break();
+				if (!Debugger.IsAttached)
+				{
+					Debugger.Launch();
+				}
+				else
+				{
+					Debugger.Break();
+				}
 			}
 		}
 	}
