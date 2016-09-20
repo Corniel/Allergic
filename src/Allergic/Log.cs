200e8a81e474e0964bc916073eef80871b3ae51f
Corniel Nobel <mail@corniel.nl>
2016-09-20 09:55:58 +0200

    Added simple file logger.

---
+++ src/Allergic/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Allergic
+{
+	/// <summary>Lightweight file log.</summary>
+	public static class Log
+	{
+		/// <summary>The file to log to.</summary>
+		public static readonly string File = Path.Combine(Path.GetTempPath(), "allergic.log");
+
+		/// <summary>Writes a message to the log file.</summary>
+		[DebuggerStepThrough]
+		public static void Write(string format, params object[] args)
+		{
+			try
+			{
+				using (var writer = new StreamWriter(File, true))
+				{
+					var prefix = DateTime.Now.ToString("HH:mm.fff ");
+					writer.WriteLine(prefix + format, args);
+				}
+
+			}
+			catch { /* it is logging, it should never fail. */ }
+		}
+	}
+}
