1299923ff2937806406227bca3ec685f2db80a5a
Corniel Nobel <corniel.nobel@exact.com>
2018-06-15 10:19:16 +0200

    Simplify solution/projects and make .NET Standard.

--- src/Allergic/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Allergic")]
-[assembly: AssemblyDescription("Simple debugger assistant.")]
-[assembly: AssemblyProduct("Allergic")]
-[assembly: AssemblyCopyright("Copyright © 2016, Corniel Nobel")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
